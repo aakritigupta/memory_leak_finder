5bbe9231ba0c7397d6d2c3066e920536f68fb809
Markus Fenske <iblue@gmx.net>
2014-01-03 11:36:19 +0100

    Initial code

---
+++ lib/memory_leak_finder.rb
@@ -0,0 +1,24 @@
+require 'objspace'
+
+module MemoryLeakFinder
+  def trace
+    raise "Block required" unless block_given?
+
+    gc_params = {full_mark: true, immediate_sweep: true}.freeze
+
+    ObjectSpace.trace_object_allocations_clear
+    ObjectSpace.trace_object_allocations do
+      yield
+    end
+
+    GC.start(gc_params)
+
+    leaks = false
+    ObjectSpace.each_object do |o|
+      next unless ObjectSpace.allocation_sourcefile(o)
+      leaks = true
+      puts "LEAK! #{o.class} #{o.inspect} allocated at #{ObjectSpace.allocation_sourcefile(o)}:#{ObjectSpace.allocation_sourceline(o)}: approx. #{ObjectSpace.memsize_of(o)} bytes"
+    end
+    leaks
+  end
+end
---
+++ memory_leak_finder.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'memory_leak_finder'
+  s.version     = '0.0.1'
+  s.date        = '2014-01-03'
+  s.summary     = "Memory leak finder"
+  s.description = "A simple gem to find memory leaks"
+  s.authors     = ["Markus Fenske"]
+  s.email       = 'markus.fenske@blacklane.com'
+  s.files       = ["lib/memory_leak_finder.rb"]
+  s.homepage    = 'http://rubygems.org/gems/memory_leak_finder'
+  s.license     = 'MIT'
+end
